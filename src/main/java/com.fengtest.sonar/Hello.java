cb9e9ca545267cba3cae82bc006dfeb2e659125e
Feng QIN <qinfeng0152@gmail.com>
2018-10-08 00:27:29 +0200

    add source

---
+++ src/main/java/com.fengtest.sonar/Hello.java
@@ -0,0 +1,20 @@
+package com.fengtest.sonar;
+
+public class Hello {
+
+    public String sayHello(String name){
+        if("feng".equals(name))
+            return "Hello "+ name;
+
+        return "dont konw u!";
+
+    }
+
+    public String sayHello2(String name){
+        if("feng".equals(name))
+            return "Hello "+ name;
+
+        return "dont konw u!";
+
+    }
+}
