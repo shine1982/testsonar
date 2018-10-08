19aefdf6d58cd3e7610134630d3178e19ee8b67d
Feng QIN <qinfeng0152@gmail.com>
2018-10-09 00:31:42 +0200

    add sonar

---
+++ src/main/java/com.fengtest.sonar/World.java
@@ -0,0 +1,13 @@
+package com.fengtest.sonar;
+
+public class World {
+
+    public String sayHello(String name){
+        if("feng".equals(name))
+            return "Hello "+ name;
+
+        return "dont konw u!";
+
+    }
+
+}
